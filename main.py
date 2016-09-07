6ed6ac7568db412ab3f3c1c95425001bc14e1cf0
Santi871 <santivegega@gmail.com>
2016-09-06 23:53:43 -0300

    set api_request_delay to 1

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     r = praw.Reddit("windows:ModlogAnalyzer v0.1 by /u/Santi871 - copy modlog to database")
     o = OAuth2Util(r)
     o.refresh(force=True)
+    r.config.api_request_delay = 1
     logger = ModlogLogger(r, 'explainlikeimfive')
 
     while True:
