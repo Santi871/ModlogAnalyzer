e19caa840661270220db8ad6285fe6293f78fdca
Santi871 <santivegega@gmail.com>
2016-09-06 23:52:57 -0300

    added HTTP error catching

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import praw
 from OAuth2Util import OAuth2Util
 from reddit_interface.modlog_analyzer import ModlogLogger
+import requests.exceptions
+from time import sleep
 
 if __name__ == "__main__":
     r = praw.Reddit("windows:ModlogAnalyzer v0.1 by /u/Santi871 - copy modlog to database")
     o = OAuth2Util(r)
     o.refresh(force=True)
     logger = ModlogLogger(r, 'explainlikeimfive')
-    logger.pull_modlog()
+
+    while True:
+        try:
+            logger.pull_modlog()
+        except (praw.errors.HTTPException, requests.exceptions.HTTPError):
+            sleep(2)
+
